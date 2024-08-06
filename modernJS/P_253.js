a634c7be7a2d290ae4361bd9f506bacc2ba3dd79
saul <dlathf3210@naver.com>
2024-08-06 21:16:48 +0900

    0806

---
+++ modernJS/P_253.js
@@ -0,0 +1,10 @@
+function multiply(x, y){
+    console.log();
+    return x * y; 
+}
+
+// console.log(multiply()); 
+// console.log(multiply(1)); 
+// console.log(multiply(2, 3));
+// console.log(multiply(4, 5, 6));
+console.log(multiply.__proto__);
