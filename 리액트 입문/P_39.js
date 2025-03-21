b6111de0256e43b8a2c2b21f9936152c9da67d86
Deskmini-X300\User-6 <dlathf3210@naver.com>
2025-03-21 17:26:01 +0900

    20250321

---
+++ 리액트 입문/P_39.js
@@ -0,0 +1,6 @@
+let val2 = "let 변수";
+console.log(val2);
+
+// let val2 = "s";
+// console.log(val2);
+// let은 재선언 불가 
---
+++ 리액트 입문/P_40.js
@@ -0,0 +1,4 @@
+const val3 = "const 변수";
+
+// val3 = "덮어쓰기";
+// const 엎어 쓰기 불가능
